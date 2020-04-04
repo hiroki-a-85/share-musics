0accbf15292c873f4ee11b7ca9c13350862baddd
hiroki.a <hiroki.a.git@gmail.com>
2020-04-04 12:58:50 +0000

    user registration and authentication

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,8 +9,18 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link">サインイン</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                @if (Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">マイページ</a></li>
+                    <li class="nav-item">{!! link_to_route('logout.get', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
+                
+                <!-- \Request::fullUrl() : 現在のURLを取得 -->
+                <!-- route('...') : 指定された名前付きルートへのURLを生成 -->
+                <!-- 現在のURLが、signup.get(サインアップページ)またはlogin(ログインページ)の時は何も表示しない -->
+                @elseif (\Request::fullUrl() == route('signup.get') || \Request::fullUrl() == route('login'))
+                @else
+                    <li class="nav-item">{!! link_to_route('signup.get', 'サインアップ', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,12 @@
 @section('content')
     <div class="top-wrapper col-sm-10 offset-sm-1">
         <h3>お気に入りの音楽を共有<br>新しい音楽の発見</h3>
+        @if (Auth::check())
+        @else
+            <div class="d-flex  justify-content-center mt-3">
+                {!! link_to_route('signup.get', 'サインアップ', [], ['class' => 'btn btn-primary d-inline-block mr-5']) !!}
+                {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-primary d-inline-block pl-4 pr-4 ml-5']) !!}
+            </div>
+        @endif
     </div>
 @endsection
