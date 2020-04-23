02a1b6ec8d9295f4fc1a5038c224a5c0de10c099
hiroki.a <hiroki.a.git@gmail.com>
2020-04-23 09:16:28 +0000

    UploadSizeCheckMiddleware.php fixed, to fit to default php.ini

--- app/Http/Middleware/UploadSizeCheckMiddleware.php
+++ app/Http/Middleware/UploadSizeCheckMiddleware.php
@@ -16,26 +16,26 @@ class UploadSizeCheckMiddleware
     public function handle($request, Closure $next)
     {
         //phpinfo()よりphp.ini設定情報を確認
-        //post_max_size	20M
-        //upload_max_filesize 20M　と変更
+        //post_max_size	8M
+        //upload_max_filesize 2M
         
-        //ini_get('post_max_size'):"20M"と取得
-        //mb_substr(ini_get('post_max_size'),0,2):"20"と取得
-        //20Mは20Mバイト
+        //ini_get('upload_max_filesize'):"2M"と取得
+        //mb_substr(ini_get('upload_max_filesize'),0,2):"2"と取得
+        //2Mは2Mバイト
         
         //PHP:リクエストデータ量をバイトで取得する - $_SERVER['CONTENT_LENGTH']
         
         if(isset($_SERVER['CONTENT_LENGTH'])){
             
-            $post_max_size = (int)mb_substr(ini_get('post_max_size'),0,2) * 1024 * 1024;
+            $post_max_size = (int)mb_substr(ini_get('upload_max_filesize'),0,1) * 1024 * 1024;
             $uploaded_size = intval($_SERVER['CONTENT_LENGTH']);                          
             
             if($post_max_size < $uploaded_size ){
                 //with()
                 //新しいページヘリダイレクトした後、
                 //セッションへ保存したフラッシュデータのメッセージを取り出して、表示できる
-                //ビューではsession('message_post_max_size')で表示させる
-                return back()->with('message_post_max_size', 'This file is too large.');
+                //ビューではsession('message_upload_max_filesize')で表示させる
+                return back()->with('message_upload_max_filesize', 'This file is too large.');
             }  
         } 
         
--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,10 +1,10 @@
-@if (count($errors) > 0 || session('message_post_max_size'))
+@if (count($errors) > 0 || session('message_upload_max_filesize'))
     <ul class="alert alert-danger" role="alert">
         @foreach ( $errors->all() as $error )
             <li class="ml-4">{{ $error }}</li>
         @endforeach
-        @if (session('message_post_max_size'))
-            <li class="ml-4">{{ session('message_post_max_size') }}</li>
+        @if (session('message_upload_max_filesize'))
+            <li class="ml-4">{{ session('message_upload_max_filesize') }}</li>
         @endif
     </ul>
 @endif
