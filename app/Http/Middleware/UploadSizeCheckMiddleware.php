ca5f593439677ef6b50cb253df5a1c41b94cd843
hiroki.a <hiroki.a.git@gmail.com>
2020-04-23 02:37:20 +0000

    UploadSizeCheckMiddleware.php fixed

--- app/Http/Middleware/UploadSizeCheckMiddleware.php
+++ app/Http/Middleware/UploadSizeCheckMiddleware.php
@@ -23,19 +23,21 @@ class UploadSizeCheckMiddleware
         //mb_substr(ini_get('post_max_size'),0,2):"20"と取得
         //20Mは20Mバイト
         
-        $post_max_size = mb_substr(ini_get('post_max_size'),0,2) * 1024 * 1024;
+        //PHP:リクエストデータ量をバイトで取得する - $_SERVER['CONTENT_LENGTH']
         
-        //PHP:リクエストデータ量をバイトで取得する - $request->server('CONTENT_LENGTH')
-        
-        $uploaded_size = $request->server('CONTENT_LENGTH');
-        
-        if($post_max_size < $uploaded_size ){
-            //with()
-            //新しいページヘリダイレクトした後、
-            //セッションへ保存したフラッシュデータのメッセージを取り出して、表示できる
-            //ビューではsession('message_post_max_size')で表示させる
-            return back()->with('message_post_max_size', 'This file is too large.');
-        }      
+        if(isset($_SERVER['CONTENT_LENGTH'])){
+            
+            $post_max_size = (int)mb_substr(ini_get('post_max_size'),0,2) * 1024 * 1024;
+            $uploaded_size = intval($_SERVER['CONTENT_LENGTH']);                          
+            
+            if($post_max_size < $uploaded_size ){
+                //with()
+                //新しいページヘリダイレクトした後、
+                //セッションへ保存したフラッシュデータのメッセージを取り出して、表示できる
+                //ビューではsession('message_post_max_size')で表示させる
+                return back()->with('message_post_max_size', 'This file is too large.');
+            }  
+        } 
         
         return $next($request);
     }
